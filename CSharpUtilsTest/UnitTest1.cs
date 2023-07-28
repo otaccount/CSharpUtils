7d4ac08417b4f78e82bf24a1295ff35a6ef52b8c
oota <oota.123@gmail.com>
2023-07-28 22:37:38 +0900

    add TestProject

---
+++ CSharpUtilsTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharpUtilsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void テスト０１()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ CSharpUtilsTest/utils/UtilsTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharpUtilsTest.utils
+{
+    [TestClass]
+    public class UtilsTest
+    {
+        [TestMethod]
+        public void テスト０１()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
